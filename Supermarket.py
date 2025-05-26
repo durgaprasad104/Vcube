e8965a2c67f73372db3cf37ae319e47a852d3655
Durgaprasad <128016655+durgaprasad104@users.noreply.github.com>
2025-05-26 10:39:47 +0530

    Create Supermarket.py

---
+++ Supermarket.py
@@ -0,0 +1,29 @@
+def market(items,cost,stock,a):
+    if a==1:
+        for i in range (len(items)):
+            print(f"{items[i],cost[i],stock[i]}")
+    elif a==2:
+        b=input("Enter the Product name: ")
+        c=int(input("Enter the choice 1.Update Price or 2.Update Stock :")) #update price
+        if c==1:
+            if b in items: #milk
+                d=items.index(b) #0
+                e=int(input("Enter the Updated Price for this product: ")) #e ==90
+                cost[d]=e
+                for i in range (len(items)):
+                    print(f"{items[i],cost[i],stock[i]}")
+        elif c==2:
+            if b in items:
+                g=items.index(b)
+                h=int(input("Enter the Updated stock number: "))
+                stock[g]=h
+                for i in range (len(items)):
+                    print(f"{items[i],cost[i],stock[i]}")
+    else:
+        print("Invalid Choice")
+    #return
+items=['Milk','Bread','Rice','Cooldrinks','Curd']
+cost=[60,30,600,100,30]
+stock=[10,8,9,11,7]
+a=int(input("Enter the choice: 1.Display Data  or 2.Update Data :"))
+market(items,cost,stock,a)
